4e7de51f7c7fd7f61323adf19b76da22a5f33af3
Sharad Vikram <sharad.vikram@gmail.com>
2013-03-05 19:23:03 -0800

    added in reportResources and reportConnections

--- test/fetcherTest.js
+++ test/fetcherTest.js
@@ -1,32 +1,34 @@
 describe('fetcher', function(){
   PeerClient = function(options) {
     if (!(this instanceof PeerClient)) return new PeerClient(options);
-    
+
     EventEmitter.call(this);
 
     var self = this;
-    
+
     options = util.extend({
       debug: false
     }, options);
     this._options = options;
-    
-    
+
+
     // This connects to the PeerCDN server
-    this._socket = new Socket();
-    
+    var host = "localhost";
+    var port = 8080;
+    this._socket = new Socket(host, port);
+
     // Wait for list of peers from server
     this._socket.on('resource', function(url, peers){
       // Download image from Peers
 
     });
     this.send = function(msg){
-    
+
     };
     setTimeout(function(){
       self.emit('open');
     },0);
-    
+
   }
 
 
@@ -34,25 +36,25 @@ describe('fetcher', function(){
   util.inherits(PeerClient, EventEmitter);
 
 
-  
+
   /*
   function PeerFetcher2(file, peers, options) {
     PeerFetcher.call(this, file, peers, options); //call super constructor.
   }
-   
+
   PeerFetcher2.prototype = Object.create(PeerFetcher.prototype);
   */
 
-  
 
- 
+
+
   describe('_init', function(){
     it ('should init without errors', function(done){
       var testPeerFetcher = new PeerFetcher({name: 'herp.psd', size: '1024'}, [{}, {}, {}],{chunkSize:1000});
       expect(testPeerFetcher._chunks.length).to.be(2);
       var expectedChunks = [{start:0 , end:999} , {start:1000, end:1023}];
       expect(testPeerFetcher._chunks).to.eql(expectedChunks);
-      done();    
+      done();
     })
   });
 
@@ -63,14 +65,14 @@ describe('fetcher', function(){
       expect(testPeerFetcher._chunks.length).to.be(2);
       var expectedChunks = [{start:0 , end:799} , {start:800, end:1599}];
       expect(testPeerFetcher._chunks).to.eql(expectedChunks);
-      done();    
+      done();
     });
     it ('works for fileSize one less than maxChunkSize', function(done){
       var testPeerFetcher = new PeerFetcher({name: 'herp.psd', size: '799'}, [{}, {}, {}]);
       expect(testPeerFetcher._chunks.length).to.be(1);
       var expectedChunks = [{start:0 , end:798}];
       expect(testPeerFetcher._chunks).to.eql(expectedChunks);
-      done();    
+      done();
     });
     it('works for fileSize one greater than maxChunkSize', function(done){
       var testPeerFetcher = new PeerFetcher({name: 'herp.psd', size: '801'}, [{}, {}, {}]);
@@ -99,24 +101,24 @@ describe('fetcher', function(){
         for(var i = 800; i < 1024; i++){
           expect(intArray[i]).to.equal(testIntArray[i-800]);
         }
-       
+
         done();
       }, 0);
     });
   });
   describe('_invalidPeer', function(){
     it('', function(){
-      
+
     });
   });
   describe('_next', function(){
     it('', function(){
-      
+
     });
   });
   describe('_wakeAllPeers', function(){
     it('', function(){
-      
+
     });
   });
 });
